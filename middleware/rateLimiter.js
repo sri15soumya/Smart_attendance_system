bc30bef1c005bc2eaaa9c02ff426203cd26ac177
B Sri Soumya <sri09soumya@gmail.com>
2026-08-22 11:57:57 +0530

    Update rateLimiter.js

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@
 const rateLimit=require("express-rate-limit")
 
 const rateLimiter=rateLimit({
-    windowMS:15*60*1000,
+    windowMs:15*60*1000,
     max:100,
     message:{
         msg:"Too many requests.Try again later"
